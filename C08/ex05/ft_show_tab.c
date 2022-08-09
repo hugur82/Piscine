60b37d676f44ce3de3c0ffe887932a410e5e20f5
khuq <ugur_hakan@yahoo.fr>
2022-08-09 21:52:45 +0200

    exo 5

---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,19 @@
+#include "ft_strs_to_tab.c"
+
+void ft_show_tab(struct s_stock_str *par)
+{
+    int i;
+
+    i = 0;
+    while(0 != par[i].str)
+    {
+        write ( 1, par[i].str, par[i].size);
+        i++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    
+    ft_show_tab(ft_strs_to_tab(argc,argv));
+}
